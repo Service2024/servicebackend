17f35497c9e4b4a11dcf34eb6cee5b4fea3e8901
vschool4566 <serviceharbour2024@gmail.com>
2024-08-07 09:33:42 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -17,11 +17,11 @@ module.exports={
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DATABASE_USERNAME,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": process.env.DATABASE_HOST,
-    "port": process.env.DATABASE_PORT,
+    "username": "service_database_vtto_user",
+    "password": "gY7HGU9IAfhQfebIAfsdV461UBeKAUXB",
+    "database": "service_database_vtto",
+    "host":"dpg-cqpnc83v2p9s73cftqo0-a",
+    "port":5432,
     "dialect": "postgres"
   }
 }
